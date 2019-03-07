9f68e8d9dca7756970956ea8927c04e8a3f0ad8a
Kirk VanArkel <kirk.vanarkel@colorado.edu>
2019-03-07 04:21:18 +0000

    unpack final + visualize

--- unpack.sh
+++ unpack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-for f in ~/work/week6/cwl-data/data/structured/*.tar.gz; do tar zxvf "$f"; done
+for f in ~/work/week6/cwl-data/data/structured/structured-2018*.tar.gz; do tar zxvf "$f"; done
 
